2f48809b63a9b1908e28ad9d26ed5e784280281f
balajinani <balajipunee@gmail.com>
2022-09-13 21:31:56 +0530

    Added: Check Single Player At Start Position Zero

---
+++ src/SnakeAndLadderUC1.java
@@ -0,0 +1,6 @@
+public class SnakeAndLadderUC1 {
+    public static void main(String[] args) {
+        int startPosition = 0;
+        System.out.println("Game starts and Player is at Position : " + startPosition);
+    }
+}
