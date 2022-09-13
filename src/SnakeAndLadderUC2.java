17345e20f1abb8e9ed38d5ad190650c70b833c0e
balajinani <balajipunee@gmail.com>
2022-09-13 21:40:38 +0530

    Added: Check Number Between One To Six

---
+++ src/SnakeAndLadderUC2.java
@@ -0,0 +1,8 @@
+public class SnakeAndLadderUC2 {
+    public static void main(String[] args) {
+        int startPosition = 0;
+        System.out.println("Game starts and Player is at Position : " + startPosition);
+        int dice = (int) Math.floor(Math.random() * 6 + 1);
+        System.out.println("The Number on dice is " + dice);
+    }
+}
